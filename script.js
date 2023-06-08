bab2ab9e7c15be35b8b0650d8886fd39ed6ef1b6
himalakshmi <124651043+himalakshmi@users.noreply.github.com>
2023-06-08 10:04:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
 
 function palindrome(str){
-
+str = str.toLowerCase();
+let  left = 0;
+let right = str.length-1;
+while(left<right){
+	if(str[left]  !== str[right]){
+		return false;
+	}
+	left++;
+	right--;
+}	
+	return true;
 }
+
 module.exports = palindrome
